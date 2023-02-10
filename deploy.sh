cd129edf3f80cf32c0e6f9debb830645b810d063
Talandar99 <grzegorzc66@gmail.com>
2023-02-10 10:35:45 +0100

    cut deploy in half

--- deploy.sh
+++ deploy.sh
@@ -11,23 +11,3 @@ echo "composing up backend"
 echo "-----------------------------------"
 cd phoenix_shop_api
 docker compose up -d --build
-sleep 5
-echo "waiting"
-sleep 5
-echo "db is starting"
-sleep 5
-echo "-----------------------------------"
-echo "initializing and migrating database"
-echo "-----------------------------------"
-./init_database.sh
-echo "-----------------------------------"
-echo "loading database"
-echo "-----------------------------------"
-cd ..
-./curl_load_database.sh
-
-echo "-----------------------------------"
-echo "frontend is running on port 8888"
-echo "phoenix api is running on port 4000"
-echo "postgresql is running on port 5432"
-echo "-----------------------------------"
